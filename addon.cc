19531bf45d7d2bbec6c48a33971312a5965f77ab
JckXia <ys3xia@uwaterloo.ca>
2023-07-29 18:55:14 -0400

    Adding some annotations

--- addon.cc
+++ addon.cc
@@ -1,34 +1,40 @@
 #include <napi.h>
 
-
 Napi::Function GetJsRequireFunction(const Napi::CallbackInfo &info) 
 {
   Napi::Env env = info.Env();
-
   Napi::Object module_object = info[0].As<Napi::Object>();
-  Napi::String module_name = Napi::String::New(env, "path");
   Napi::Function require =  module_object.Get("require").As<Napi::Function>();
   return require;
 }
-Napi::Value CallRequire(const Napi::CallbackInfo& info) {
-  Napi::Env env = info.Env();
 
-  Napi::Object module_object = info[0].As<Napi::Object>();
+// Small POC to demonstrate using `path.join` from C++
+Napi::Value CallJoinFromCppLand(const Napi::CallbackInfo& info)
+{
+
+  Napi::Env env = info.Env();
   Napi::String module_name = Napi::String::New(env, "path");
-  Napi::Function require =  module_object.Get("require").As<Napi::Function>();
-  
-  // The
+  Napi::Function require = GetJsRequireFunction(info);
+
+  // Get the 'path' corelib from node, as an Object
   Napi::Object jsPathLib = require.Call({module_name}).As<Napi::Object>();
   
+  // Get the `join` function from the path object. This has to be a Napi::Function type
   Napi::Function pathJoin = jsPathLib.Get("join").As<Napi::Function>();
 
-  Napi::String pathArgs = Napi::String::New(env, "arggh");
+  // Prepare args
+  Napi::String protcol = Napi::String::New(env, "https");
   Napi::String domain = Napi::String::New(env, "pagerduty");
-  Napi::Value results = pathJoin.Call({pathArgs, domain});
+  Napi::String uri = Napi::String::New(env, "api");
+  
+  // invoke the `join` function and provide it with some arguments
+  //    -> Functions can be invoked with either initialization lists or else.
+  Napi::Value results = pathJoin.Call({protcol, domain, uri});
   return results;
 }
+
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
-  exports.Set("js_require", Napi::Function::New(env,CallRequire));
+  exports.Set("js_require", Napi::Function::New(env,CallJoinFromCppLand));
   return exports;
 }
 
--- entry_point.js
+++ entry_point.js
@@ -1,8 +1,9 @@
 var addon = require('./build/Release/hello.node')
 
-const mock_object = {
-    a: 4,
-    test_func: () => 290
-};
+// The article linked talked a little bit about commonjs module implementations.
+// IIUC The `require` is a function attached to the `module` object. To use it we need to 
+// hand this off to node-addon-api. 
 
+// This will print out https/pagerduty/api, which acts
+// as if we invoked path.join('https','pagerduty','api') from js
 console.log(addon.js_require(module));
